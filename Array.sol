0a8111e1393327884c32987a0d41e241667970ad
Muhammad Kamran Shahid <137478790+kamransd@users.noreply.github.com>
2023-07-04 14:18:40 +0500

    Update Array.sol

--- Array.sol
+++ Array.sol
@@ -1,3 +1,4 @@
+Step no.1
 pragma solidity ^0.8.0;
 
 contract array {
@@ -7,3 +8,31 @@ contract array {
     }
 }
 // output show 6
+
+
+Step no.2
+
+pragma solidity ^0.8.0;
+
+contract Array {
+
+    uint[] public number;
+    // Any Value Store In Array Used Push Keyword
+    function addElement(uint num) public {
+        number.push(num);
+    }
+    //Total Length Of Array Value Used Length Keyword
+    function size()public view returns(uint){
+        return number.length;
+    }
+    //Delete Any Value of Array That Store In Array
+    function remove(uint index)public {
+        delete number[index];
+    }
+    //Last Value Store In Array Are Remove With The The Help Of Pop Keyword
+    function getarr() public {
+        number.pop(); 
+    }
+    
+}
+
