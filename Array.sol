1f955d7d97c2aa4216543871103d6e5002a4bf7d
Muhammad Kamran Shahid <137478790+kamransd@users.noreply.github.com>
2023-07-04 14:20:14 +0500

    Update Array.sol

--- Array.sol
+++ Array.sol
@@ -36,3 +36,17 @@ contract Array {
     
 }
 
+Step no 3.
+
+pragma solidity ^0.8.0;
+
+contract array {
+    uint[]public num = [1,2,45,67,89,0];
+    function getArr()public returns(uint){
+        num[0] = 3;
+        return num[0];
+
+    }
+}
+
+
