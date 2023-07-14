870097bf8c59006060d1f932184117807889a683
Muhammad Kamran Shahid <137478790+kamransd@users.noreply.github.com>
2023-07-14 09:04:09 +0500

    Create Government.sol

---
+++ Government.sol
@@ -0,0 +1,21 @@
+pragma solidity^0.8.0;
+
+contract Government {
+
+    struct Transaction {
+        address from;
+        address to;
+        uint amount;
+        uint txid;
+    }
+
+    Transaction[] transactionLog;
+
+    function addTransaction(address _from, address _to, uint _amount) external {
+        Transaction memory _transaction = Transaction(_from, _to, _amount, transactionLog.length);
+        transactionLog.push(_transaction);
+    } 
+    function getTransaction(uint _index) public view returns(address, address, uint){
+        return (transactionLog[_index].from, transactionLog[_index].to, transactionLog[_index].amount);
+    }
+}
